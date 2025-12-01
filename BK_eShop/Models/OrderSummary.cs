4dbe84fe2676a40960fe05e80ae3bb0975ecc320
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2025-12-01 18:01:03 +0100

    Added ShopContext

--- BK_eShop/Models/OrderSummary.cs
+++ BK_eShop/Models/OrderSummary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace BK_eShop.Models
 {
-    internal class OrderSummary
+    /*
+     * Detta är en keyless entitet (ingen PK)
+     * Den representerar en SQL view, en spara Select-query
+     * Vi använder dessa Views i EF Core som gör att den kan läsa den precis som en vanlig tabell
+    */
+
+    [Keyless]
+    public class OrderSummary
     {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public decimal TotalAmount { get; set; }
     }
 }
---
+++ BK_eShop/ShopContext.cs
@@ -0,0 +1,81 @@
+﻿using BK_eShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BK_eShop
+{
+    public class ShopContext : DbContext
+    {
+        // Gör alla DBSet
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderRow> OrderRows => Set<OrderRow>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<OrderSummary> OrderSummaries => Set<OrderSummary>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = Path.Combine(AppContext.BaseDirectory, "shop.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(c =>
+            {
+                // PK
+                c.HasKey(x => x.CustomerId);
+
+                // Properties
+                c.Property(x => x.CustomerName).IsRequired().HasMaxLength(150);
+                c.Property(x => x.CustomerPhone).IsRequired();
+                c.Property(x => x.CustomerEmail).IsRequired().HasMaxLength(150);
+
+                c.HasIndex(x => x.CustomerEmail).IsUnique();
+            });
+
+            modelBuilder.Entity<Order>(o =>
+            {
+                // PK
+                o.HasKey(x => x.OrderId);
+
+                // Properties
+                o.Property(x => x.OrderDate).IsRequired();
+                o.Property(x => x.OrderStatus).IsRequired();
+                o.Property(x => x.OrderTotalAmount).IsRequired();
+            });
+
+            modelBuilder.Entity<OrderRow>(r =>
+            {
+                // PK
+                r.HasKey(x => x.OrderRowId);
+
+                // Properties
+                r.Property(x => x.OrderRowQuantity).IsRequired();
+            });
+
+            modelBuilder.Entity<Product>(p =>
+            {
+                // PK
+                p.HasKey(x => x.ProductId);
+
+                // Properties
+                p.Property(x => x.ProductName).IsRequired().HasMaxLength(150);
+                p.Property(x => x.ProductPrice).IsRequired();
+                p.Property(x => x.ProductStock).IsRequired();
+            });
+
+
+            /*// Order Summary
+            modelBuilder.Entity<OrderSummary>(e =>
+            {
+                e.HasNoKey(); // Saknar PK
+                e.ToView("OrderSummaryView"); // Kopplar tabellen mot SQL
+            });*/
+        }
+    }
+}
