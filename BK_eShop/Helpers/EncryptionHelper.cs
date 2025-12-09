ee3ffc319e12b7b97e3f2d392431000015e0d829
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2025-12-09 11:16:35 +0100

    Created EncryptionHelper

---
+++ BK_eShop/Helpers/EncryptionHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using BK_eShop.Models;
+
+namespace BK_eShop.Helpers
+{
+    public class EncryptionHelper
+    {
+        private const byte Key = 0x42; // 66 bytes
+
+        // Encrypt
+        public static string Encrypt(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            // Convert text to bytes
+            var bytes = System.Text.Encoding.UTF8.GetBytes(text);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte)(bytes[i] ^ Key);
+            }
+
+            // Save result as text
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static string Decrypt(string encyptedText)
+        {
+            if (string.IsNullOrEmpty(encyptedText))
+            {
+                return encyptedText;
+            }
+        
+
+        // Redo text to bytes
+        var bytes = Convert.FromBase64String(encyptedText);
+
+        for (int i = 0; i<bytes.Length; i++)
+        {
+            bytes[i] = (byte) (bytes[i] ^ Key);
+        }
+
+        return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
